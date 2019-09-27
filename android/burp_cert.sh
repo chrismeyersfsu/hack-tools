4be5e0538d6ca397dcb2dd67fe0c3050e594f317
chris meyers <chris.meyers.fsu@gmail.com>
2019-09-26 21:34:54 -0400

    add burp suite cert pusher

---
+++ android/burp_cert.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+openssl x509 -inform DER -in $1 -out cacert.pem
+CERT_HASH=`openssl x509 -inform PEM -subject_hash_old -in cacert.pem |head -1`
+mv cacert.pem ${CERT_HASH}.0
+adb root
+adb remount
+adb shell mkdir -p /system/etc/security/cacerts/
+adb push ${CERT_HASH}.0 /system/etc/security/cacerts/
